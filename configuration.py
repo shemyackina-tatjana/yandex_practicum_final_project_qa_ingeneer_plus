7f5da2f385052ee9d39ca03ba305c1d3408738c2
shemyackina-tatjana <shemyackina.tatjana2015@yandex.ru>
2023-06-03 17:22:35 +0100

    Тест

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Татьяна Шемякина, 5-я когорта - Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://db85a545-7e6e-4ee1-95a8-c0bc9e89c304.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
